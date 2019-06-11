d0a40f73432435f0735654b61685b22dfb8e0e67
dlakshmip18 <51689291+dlakshmip18@users.noreply.github.com>
2019-06-11 10:36:48 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<malloc.h>
+int main()
+{
+	int n,*arr,itr;
+	long long int num=0;
+	scanf("%d",&n);
+	arr=(int*)malloc(n*sizeof(int));
+	for(itr=0;itr<n;itr++)
+	scanf("%d",&arr[itr]);
+	for(itr=0;itr<n;itr++)
+	if(itr==arr[itr])
+	printf("%d ",arr[itr]);
+}
